cb86b912fb87281ad76a57ee3535e941d4ffd388
dbozarth <don@fuelcnc.com>
2019-01-17 16:14:39 -0600

    Set up data defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,14 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ror_items, -> { where(subtitle: 'Ruby On Rails') }
+
+  # Setting defaults allows for non-required data to be set to default values
+  # when the action is called in the controller. ||= means if nil use default.
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
+
 end
