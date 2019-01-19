11c4962fec043834357f951e9352f45a8256d9e3
dbozarth <don@fuelcnc.com>
2019-01-19 08:33:35 -0600

    Created layout files for blogs and portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,9 @@
 class PortfoliosController < ApplicationController
+layout 'portfolio'
+
   def index
     @portfolio_items = Portfolio.all
+    @page_title = "My Portfolio Page"
   end
 
   def angular
