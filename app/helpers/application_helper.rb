f41f1f2a713c7f778570b258878171a549e84288
dbozarth <don@fuelcnc.com>
2019-01-21 09:53:27 -0600

    Integrated a fuctional permissions system with pertergate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,13 @@ module ApplicationHelper
   ## Will render on views as html. 
   ## Removes redundant code for common items.
   def login_helper
-     if current_user.is_a?(User)
-       link_to "Logout", destroy_user_session_path, method: :delete, class: 'btn btn-primary'
-     else
+     if current_user.is_a?(GuestUser)
       ## Chaining of these with + will concatenate the calls
       ## preventing Ruby from only returning the last item!
       (link_to "Register", new_user_registration_path, class: 'btn btn-success') + "&nbsp;&nbsp;&nbsp;".html_safe +
       (link_to "Login", new_user_session_path, class: 'btn btn-secondary')
+     else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: 'btn btn-primary'
      end
   end
 
