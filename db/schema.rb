c7ff65897656cc733caea50335ce652f5b05d161
dbozarth <don@fuelcnc.com>
2019-01-21 08:28:04 -0600

    Removed petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_144007) do
+ActiveRecord::Schema.define(version: 2019_01_21_142541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
