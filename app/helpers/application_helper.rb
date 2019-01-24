6cd8be8d0bcf9be8b4155bf5fbf84faf81776bf0
dbozarth <don@fuelcnc.com>
2019-01-24 05:37:34 -0600

    Set up Devise Forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,20 +2,20 @@ module ApplicationHelper
   
   ## Will render on views as html. 
   ## Removes redundant code for common items.
-  def login_helper
+  def login_helper style
      if current_user.is_a?(GuestUser)
       ## Chaining of these with + will concatenate the calls
       ## preventing Ruby from only returning the last item!
-      (link_to "Register", new_user_registration_path, class: 'btn btn-success') + "&nbsp;&nbsp;&nbsp;".html_safe +
-      (link_to "Login", new_user_session_path, class: 'btn btn-secondary')
+      (link_to "Register", new_user_registration_path, class: style) +
+      (link_to "Login", new_user_session_path, class: style)
      else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: 'btn btn-primary'
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
      end
   end
 
   def source_helper(layout_name)
     if session[:source]
-      greeting = "Thanks for visiting from #{session[:source]} and you are on the #{layout_name} layout"
+      greeting = "Thanks for visiting from <strong>#{session[:source]}</strong> and you are on the #{layout_name} layout".html_safe
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
